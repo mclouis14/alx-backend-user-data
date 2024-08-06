832424a34aee618d7ad1317789155eb751ee126e
mclouis14 <louis.officialz@gamil.com>
2024-08-06 15:21:30 -0700

    Error handler:Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,11 +14,37 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.before_request
+def filter_request():
+    if auth is None:
+        return
+
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(401)
+def unauthorized_request(error) -> str:
+    '''Unauthorized requests
+    '''
+
+    return jsonify({
+        "error": "Unauthorized"
+    }), 401
 
 
 if __name__ == "__main__":
